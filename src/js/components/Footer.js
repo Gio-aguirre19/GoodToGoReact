7fcbcf5e9b1a8088d9070a806883f45148443859
Giovani Aguirre <aguirregio19@gmail.com>
2017-02-24 23:46:35 -0800

    Ready to go react boiler plate with instructions in index html

---
+++ src/js/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div>2016</div>
+        <div>Giotest</div>
+      </footer>
+  )}
+}
---
+++ src/js/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Good To Go!</h1>
+        <div>Home</div>
+        <div>About</div>
+        <div>Contact</div>
+      </header>
+  )}
+}
