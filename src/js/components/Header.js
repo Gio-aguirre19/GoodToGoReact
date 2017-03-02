38f6415b92d6eb9c00a2ffffb8847439fa43aba9
Giovani Aguirre <aguirregio19@gmail.com>
2017-03-01 19:16:11 -0800

    Boiler plate update

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>Working fine</h1>
+        <div><h1>Working fine</h1></div>
         <div>Home</div>
         <div>About</div>
         <div>Contact</div>
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     }]
   },
   output: {
-    path: __dirname + '/src/build/',
+    path: __dirname + '/dist/',
     filename: "index.min.js"
   },
   plugins: debug ? [
